e58c9e7df810e02df0e8361a4c17208bc5a71ef9
oleh skrypnyk <oleh.skrypnyk@ukr.net>
2026-01-26 20:14:56 +0200

    Initial commit

---
+++ types/note.ts
@@ -0,0 +1,16 @@
+export interface Note {
+  readonly id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  tag: NoteTag;
+}
+
+export interface CreateNote {
+  title: string;
+  content: string;
+  tag: NoteTag;
+}
+
+export type NoteTag = "Todo" | "Personal" | "Meeting" | "Shopping" | "Work";
